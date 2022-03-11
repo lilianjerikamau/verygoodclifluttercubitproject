a368173bbb2c7121c743aca2d7a644dbda400c6b
lilianjerikamau <lilianjerikamau@gmail.com>
2022-03-11 11:24:47 +0300

    [WIP] restructured code

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -12,10 +12,10 @@ import 'package:very_good_project/app/app.dart';
 import 'package:very_good_project/app/view/app.dart';
 import 'package:very_good_project/bootstrap.dart';
 import 'package:very_good_project/cubit/post_fetch_cubit.dart';
-import 'package:very_good_project/cubit/weather_cubit.dart';
+
 import 'package:very_good_project/models/_index.dart';
 import 'package:very_good_project/cubit/lister.dart';
-import 'package:very_good_project/service/weather_repository.dart';
+
 import 'package:very_good_project/utils/_index.dart';
 
 import 'package:very_good_project/service/_index.dart';
